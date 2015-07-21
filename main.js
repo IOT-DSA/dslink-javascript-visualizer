cc0bc4ab2867ae0fc5245a6af82319f29133203d
Michael Bullington <mikebullingtn@gmail.com>
2015-07-21 02:54:12 -0400

    Add connect dialog/logic, tomorrow I need to fix a connect bug.

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-  (function(d3, DS) {
+document.addEventListener('DOMContentLoaded', function() {
+  var hasClicked = false;
 
-  })(d3, DS);
+  document.getElementById('connect-btn').onclick = function() {
+    var input = document.getElementById('broker-url').value;
+    if(hasClicked || input.trim().length == 0)
+      return;
+    hasClicked = true;
+
+    console.log('connecting');
+    console.log('url: ' + input);
+
+    var link = new DS.LinkProvider(input, 'visualizer-', {
+      isRequester: true,
+      isResponder: false
+    });
+
+    link.init().then(function() {
+      return link.connect();
+    }).then(function() {
+      return link.onRequesterReady;
+    }).then(function() {
+      console.log('connected');
+    });
+  };
 }, false);
