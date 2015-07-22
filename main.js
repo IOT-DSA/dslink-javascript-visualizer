dbcb541ef67adcfd2996cba529f30d2acac00097
Michael Bullington <mikebullingtn@gmail.com>
2015-07-21 22:39:30 -0400

    Support broker URL as query parameter as well as dialog.

--- main.js
+++ main.js
@@ -1,26 +1,69 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var hasClicked = false;
+(function() {
+  function getUrlParameters() {
+    var urlParams = {},
+        match,
+        pl = /\+/g, // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
 
-  document.getElementById('connect-btn').onclick = function() {
-    var input = document.getElementById('broker-url').value;
-    if(hasClicked || input.trim().length == 0)
-      return;
-    hasClicked = true;
+    while (match = search.exec(query))
+      urlParams[decode(match[1])] = decode(match[2]);
+    return urlParams;
+  }
 
+  // elements is an object, key is id, value is display type
+  function getElements(elements) {
+    return {
+      show: function() {
+        Object.keys(elements).forEach(function(element) {
+          document.getElementById(element).style.display = elements[element];
+        });
+      },
+      hide: function() {
+        Object.keys(elements).forEach(function(element) {
+          document.getElementById(element).style.display = 'none';
+        });
+      }
+    };
+  }
+
+  function connect(url) {
     console.log('connecting');
-    console.log('url: ' + input);
+    console.log('url: ' + url);
 
-    var link = new DS.LinkProvider(input, 'visualizer-', {
+    var link = new DS.LinkProvider(url, 'visualizer-', {
       isRequester: true,
       isResponder: false
     });
 
-    link.init().then(function() {
+    return link.init().then(function() {
       return link.connect();
     }).then(function() {
       return link.onRequesterReady;
     }).then(function() {
       console.log('connected');
     });
-  };
-}, false);
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var urlParams = getUrlParameters();
+    var elements = getElements({
+      'title': 'block',
+      'container': 'flex'
+    });
+
+    if(!urlParams.url) {
+      elements.show();
+
+      document.getElementById('connect-btn').onclick = function() {
+        var url = document.getElementById('broker-url').value;
+        connect(url).then(function() {
+          elements.hide();
+        });
+      };
+    } else {
+      connect(urlParams.url);
+    }
+  }, false);
+})();
