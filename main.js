698f2e812877ef98ab9adfc9c262f25fad96d88f
mbullington <mikebullingtn@gmail.com>
2015-07-26 18:21:18 -0400

    Attempt at cleaning up code, collapsing is now better.

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
 (function() {
   'use strict';
 
+  var util = {
+    getUrlParams: function() {
+      var urlParams = {},
+          match,
+          pl = /\+/g, // Regex for replacing addition symbol with a space
+          search = /([^&=]+)=?([^&]*)/g,
+          decode = function (s) { return decodeURIComponent(s.replace(pl, ' ')); },
+          query  = window.location.search.substring(1);
+
+      while (match = search.exec(query))
+        urlParams[decode(match[1])] = decode(match[2]);
+      return urlParams;
+    },
+    toggleElements: function(elements) {
+      return {
+        show: function() {
+          Object.keys(elements).forEach(function(element) {
+            document.getElementById(element).style.display = elements[element];
+          });
+        },
+        hide: function() {
+          Object.keys(elements).forEach(function(element) {
+            document.getElementById(element).style.display = 'none';
+          });
+        }
+      };
+    }
+  };
+
   // from Flat UI colors, with love
   // https://flatuicolors.com/
 
@@ -11,236 +40,230 @@
   // alizarin
   var COLOR_ACTION = '#e74c3c';
 
-  var cluster = d3.layout.cluster();
+  var types = {
+    colors: {
+      node: COLOR_NODE,
+      action: COLOR_ACTION,
+      value: COLOR_VALUE
+    },
+    getColor: function(d) {
+      return types.colors[types.getType(d)];
+    },
+    getType: function(d) {
+      if(d.node.configs['$type'])
+        return 'value';
+      if(d.node.configs['$invokable'])
+        return 'action';
+      return 'node';
+    }
+  };
+
+  var tree = d3.layout.tree();
 
   var diagonal = d3.svg.diagonal()
       .projection(function(d) { return [d.y, d.x]; });
 
   var zoom = d3.behavior.zoom();
 
-  var svg = d3.select('body').append('svg')
+  var svgRoot = d3.select('body').append('svg')
       .style('width', '100%')
       .style('height', '100%')
       .call(zoom.on('zoom', function() {
-        svg.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
+        svgRoot.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
         zoom.translate(d3.event.translate);
         zoom.scale(d3.event.scale);
       }))
       .append('g');
 
-  var tooltip = d3.select('body').append('div')
-      .attr('id', 'tooltip')
-      .style('display', 'none')
-      .text('tooltip');
+  var svg = svgRoot.append('g');
 
-  function getUrlParameters() {
-    var urlParams = {},
-        match,
-        pl = /\+/g, // Regex for replacing addition symbol with a space
-        search = /([^&=]+)=?([^&]*)/g,
-        decode = function (s) { return decodeURIComponent(s.replace(pl, ' ')); },
-        query  = window.location.search.substring(1);
+  var tooltip = (function() {
+    var node = d3.select('body').append('div')
+        .attr('id', 'tooltip')
+        .style('display', 'none')
+        .text('tooltip');
 
-    while (match = search.exec(query))
-      urlParams[decode(match[1])] = decode(match[2]);
-    return urlParams;
-  }
-
-  // elements is an object, key is id, value is display type
-  function getElements(elements) {
     return {
-      show: function() {
-        Object.keys(elements).forEach(function(element) {
-          document.getElementById(element).style.display = elements[element];
-        });
+      show: function(d, text) {
+        var x = ((d.y + visualizer.translateY) * zoom.scale() + zoom.translate()[0]);
+        var y = ((d.x + visualizer.translateX) * zoom.scale() + zoom.translate()[1]);
+
+        node.html(text);
+        node.style('display', 'block');
+        node.style('left', (x - (node.node().getBoundingClientRect().width / 2)) + 'px');
+        node.style('top', (y - 40) + 'px');
       },
       hide: function() {
-        Object.keys(elements).forEach(function(element) {
-          document.getElementById(element).style.display = 'none';
-        });
+        node.style('display', 'none');
       }
     };
-  }
-
-  function getColorForNode(d) {
-    return {
-      node: COLOR_NODE,
-      action: COLOR_ACTION,
-      value: COLOR_VALUE
-    }[getNodeType(d)];
-  }
-
-  function getNodeType(d) {
-    if(d.node.configs['$type'])
-      return 'value';
-    if(d.node.configs['$invokable'])
-      return 'action';
-    return 'node';
-  }
-
-  function toggle(d) {
-    if (d.children) {
-      d._children = d.children;
-      d.children = null;
-    } else {
-      d.children = d._children;
-      d._children = null;
-    }
-  }
+  }());
 
   var root;
-  function update(n) {
-    var depthSize = [1];
-
-    function depth(obj, d) {
-      if(obj.children && obj.children.length > 0) {
-        obj.children.forEach(function(child) {
-          depthSize[d] = !!depthSize[d] ? depthSize[d] + 1 : 1;
-          depth(child, d + 1);
-        });
+  var visualizer = {
+    translateY: 0,
+    translateX: 0,
+    translate: function(y, x) {
+      visualizer.translateY = y;
+      visualizer.translateX = x;
+      svg.attr('transform', 'translate(' + y + ',' + x + ')');
+    },
+    toggle: function(d) {
+      if (d.children) {
+        d._children = d.children;
+        d.children = null;
+      } else {
+        d.children = d._children;
+        d._children = null;
       }
-    }
-    depth(root, 1);
+    },
+    update: function(n) {
+      var depthSize = [1];
 
-    cluster = cluster.size([200 + (20 * Math.max.apply(Math, depthSize)), depthSize.length * 300]);
-
-    var nodes = cluster.nodes(root),
-        links = cluster.links(nodes);
-
-    svg.selectAll('.link').remove();
-    var link = svg.selectAll('.link')
-        .data(links);
-
-    link.enter().append('path')
-        .attr('class', 'link')
-        .attr('d', diagonal);
-
-    link.exit().remove();
-
-    svg.selectAll('.node').remove();
-    var node = svg.selectAll('.node')
-        .data(nodes);
-
-    var nodeEnter = node.enter().append('g')
-        .attr('class', 'node')
-        .attr('transform', function(d) { return 'translate(' + d.y + ',' + d.x + ')'; });
-
-    node.exit().remove();
-
-    var circle = nodeEnter.append('circle')
-        .attr('r', 4.5)
-        .style('fill', function(d) {
-          return getColorForNode(d);
-        })
-        .on('mouseover', function(d) {
-          var x = (d.y * zoom.scale() + zoom.translate()[0]);
-          var y = (d.x * zoom.scale() + zoom.translate()[1]);
-
-          var nodeText = 'NODE';
-
-          tooltip.html('<span style="margin-right: 8px;color: '
-              + getColorForNode(d) + ';">'
-              + getNodeType(d).toUpperCase()
-              + '</span>' + d.node.remotePath);
-          tooltip.style('display', 'block');
-          tooltip.style('left', (x - (tooltip.node().getBoundingClientRect().width / 2)) + 'px');
-          tooltip.style('top', (y - 40) + 'px');
-          d3.select(this).attr('r', 6);
-        })
-        .on('mouseout', function(d) {
-          tooltip.style('display', 'none');
-          d3.select(this).attr('r', 4.5);
-        })
-        .on('click', function(d) {
-          toggle(d);
-          update(d);
-        });
-
-    nodeEnter.append('text')
-        .attr('dx', function(d) { return d.children && d.children.length > 0 ? -8 : 8; })
-        .attr('dy', 3)
-        .style('text-anchor', function(d) { return d.children && d.children.length ? 'end' : 'start'; })
-        .text(function(d) { return d.name; });
-  }
-
-  function connect(url) {
-    var link = new DS.LinkProvider(url, 'visualizer-', {
-      isRequester: true,
-      isResponder: false
-    });
-
-    return link.connect().then(function() {
-      return link.onRequesterReady;
-    }).then(function(requester) {
-      root = {
-        name: 'conns',
-        children: [],
-        node: {
-          configs: {},
-          remotePath: '/conns'
+      function depth(obj, d) {
+        if(obj.children && obj.children.length > 0) {
+          obj.children.forEach(function(child) {
+            depthSize[d] = !!depthSize[d] ? depthSize[d] + 1 : 1;
+            depth(child, d + 1);
+          });
         }
-      };
+      }
+      depth(root, 1);
 
-      function list(path, obj, depth) {
-        var called = false;
-        return new Promise(function(resolve, reject) {
-          console.log(path);
-          requester.list(path).on('data', function(update) {
-            if(called)
-              return;
-            called = true;
+      tree = tree.size([200 + (20 * Math.max.apply(Math, depthSize)), depthSize.length * 300]);
 
-            var promises = [];
-            Object.keys(update.node.children).forEach(function(child) {
-              var node = update.node.children[child];
-              var map = {
-                name: node.configs['$name'] || child,
-                children: [],
-                node: node
-              };
+      var nodes = tree.nodes(root),
+          links = tree.links(nodes);
 
-              obj.children.push(map);
+      svg.selectAll('.link').remove();
+      var link = svg.selectAll('.link')
+          .data(links);
 
-              if(node.remotePath.split('/').length <= 3)
-                promises.push(list(node.remotePath, map, depth + 1));
-            });
+      link.enter().append('path')
+          .attr('class', 'link')
+          .attr('d', diagonal);
 
-            Promise.all(promises).then(function() {
-              resolve();
-            }).catch(function(e) {
-              reject(e);
+      link.exit().remove();
+
+      svg.selectAll('.node').remove();
+      var node = svg.selectAll('.node')
+          .data(nodes);
+
+      var nodeEnter = node.enter().append('g')
+          .attr('class', 'node')
+          .attr('transform', function(d) { return 'translate(' + d.y + ',' + d.x + ')'; });
+
+      node.exit().remove();
+
+      var circle = nodeEnter.append('circle')
+          .attr('r', 4.5)
+          .style('fill', function(d) {
+            return types.getColor(d);
+          })
+          .on('mouseover', function(d) {
+            tooltip.show(d, '<span style="margin-right: 8px;color: '
+              + types.getColor(d) + ';">'
+              + types.getType(d).toUpperCase()
+              + '</span>' + d.node.remotePath);
+            d3.select(this).attr('r', 6);
+          })
+          .on('mouseout', function(d) {
+            tooltip.hide();
+            d3.select(this).attr('r', 4.5);
+          })
+          .on('click', function(d) {
+            visualizer.toggle(d);
+            visualizer.update(d);
+          });
+
+      nodeEnter.append('text')
+          .attr('dx', function(d) { return d.children && d.children.length > 0 ? -8 : 8; })
+          .attr('dy', 3)
+          .style('text-anchor', function(d) { return d.children && d.children.length ? 'end' : 'start'; })
+          .text(function(d) { return d.name; });
+
+      visualizer.translate(-root.y, -root.x);
+    },
+    connect: function(url) {
+      var link = new DS.LinkProvider(url, 'visualizer-', {
+        isRequester: true,
+        isResponder: false
+      });
+
+      return link.connect().then(function() {
+        return link.onRequesterReady;
+      }).then(function(requester) {
+        root = {
+          name: 'conns',
+          children: [],
+          node: {
+            configs: {},
+            remotePath: '/conns'
+          }
+        };
+
+        function list(path, obj, depth) {
+          var called = false;
+          return new Promise(function(resolve, reject) {
+            console.log(path);
+            requester.list(path).on('data', function(update) {
+              if(called)
+                return;
+              called = true;
+
+              var promises = [];
+              Object.keys(update.node.children).forEach(function(child) {
+                var node = update.node.children[child];
+                var map = {
+                  name: node.configs['$name'] || child,
+                  children: [],
+                  node: node
+                };
+
+                obj.children.push(map);
+
+                if(node.remotePath.split('/').length <= 3)
+                  promises.push(list(node.remotePath, map, depth + 1));
+              });
+
+              Promise.all(promises).then(function() {
+                resolve();
+              }).catch(function(e) {
+                reject(e);
+              });
             });
           });
-        });
-      }
+        }
 
-      list('/conns', root, 1).then(function() {
-        console.log('done');
-        update(root);
+        list('/conns', root, 1).then(function() {
+          console.log('done');
+          visualizer.update(root);
+        });
+      }).catch(function(err) {
+        console.log(err.$thrownJsError ? err.$thrownJsError.stack : err.stack);
       });
-    }).catch(function(err) {
-      console.log(err.$thrownJsError ? err.$thrownJsError.stack : err.stack);
-    });
-  }
+    },
+    main: function() {
+      var params = util.getUrlParams();
+      var elements = util.toggleElements({
+        title: 'block',
+        container: 'flex'
+      });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var urlParams = getUrlParameters();
-    var elements = getElements({
-      'title': 'block',
-      'container': 'flex'
-    });
+      if(!params.url) {
+        elements.show();
 
-    if(!urlParams.url) {
-      elements.show();
-
-      document.getElementById('connect-btn').onclick = function() {
-        var url = document.getElementById('broker-url').value;
-        connect(url).then(function() {
-          elements.hide();
-        });
-      };
-    } else {
-      connect(urlParams.url);
+        document.getElementById('connect-btn').onclick = function() {
+          var url = document.getElementById('broker-url').value;
+          visualizer.connect(url).then(function() {
+            elements.hide();
+          });
+        };
+      } else {
+        visualizer.connect(params.url);
+      }
     }
-  }, false);
+  };
+
+  document.addEventListener('DOMContentLoaded', visualizer.main, false);
 })();
